4f66d6a94ab3a3859ff4d998f3b5e1a6e50ad77b
Wathyson Samuel <wathysonsamuel61@gmail.com>
2025-07-02 13:00:28 -0300

    teste git

---
+++ testeGit/src/suportGit/suportTest.java
@@ -0,0 +1,10 @@
+package suportGit;
+
+public class suportTest {
+    public static void main(String[] args) {
+
+        System.out.println("Vamos testar o git um pouco");
+        System.out.println("Começando");
+
+    }
+}
